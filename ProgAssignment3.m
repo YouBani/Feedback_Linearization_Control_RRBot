9763e55df533e51d02a50fbbadde2ca325179802
Youness Bani <91743838+YouBani@users.noreply.github.com>
2022-03-31 21:49:44 -0400

    Works

--- ProgAssignment3.m
+++ ProgAssignment3.m
@@ -11,42 +11,43 @@ a = inv(A) * b;
 
 % Coefficient matrix of the first joint
 a1 = subs(a, [to, tf, qo, qodot, qf, qfdot], [0, 10, pi, 0, 0 0]);
+
 % Coefficient matrix of the second joint
 a2 = subs(a, [to, tf, qo, qodot, qf, qfdot], [0, 10, pi/2, 0, 0 0]);
 
-% a1 = double(a1)
-% a2 = double(a2)
+cubic_trajec1 = a1(1) + a1(2)*t + a1(3)*(t^2) + a1(4)*(t^3);
+cubic_trajec2 = a2(1) + a2(2)*t + a2(3)*(t^2) + a2(4)*(t^3);
 
-cubic_trajec1 = a1(1) + a1(2)*t + a1(3)*(t^2) + a1(4)*(t^3)
-cubic_trajec2 = a2(1) + a2(2)*t + a2(3)*(t^2) + a2(4)*(t^3)
-
-eq1= theta1_ddot*(m2*l1^2 + 2*m2*cos(theta2)*l1*r2 + m1*r1^2 + m2*r2^2 + I1 + I2) - theta2_dot*(l1*m2*r2*theta1_dot*sin(theta2) + l1*m2*r2*sin(theta2)*(theta1_dot + theta2_dot)) - u1 + theta2_ddot*(m2*r2^2 + l1*m2*cos(theta2)*r2 + I2) - g*m2*r2*sin(theta1 + theta2) - g*l1*m1*sin(theta1) - g*m1*r1*sin(theta1);
-eq2= theta2_ddot*(m2*r2^2 + I2) - u2 + theta1_ddot*(m2*r2^2 + l1*m2*cos(theta2)*r2 + I2) - g*m2*r2*sin(theta1 + theta2) + l1*m2*r2*theta1_dot*sin(theta2)*(theta1_dot + theta2_dot) - l1*m2*r2*theta1_dot*theta2_dot*sin(theta2);
+eq1 = theta1_ddot*(m2*l1^2 + 2*m2*cos(theta2)*l1*r2 + m1*r1^2 + m2*r2^2 + I1 + I2) - theta2_dot*(l1*m2*r2*theta1_dot*sin(theta2) + l1*m2*r2*sin(theta2)*(theta1_dot + theta2_dot)) - u1 + theta2_ddot*(m2*r2^2 + l1*m2*cos(theta2)*r2 + I2) - g*m2*r2*sin(theta1 + theta2) - g*l1*m1*sin(theta1) - g*m1*r1*sin(theta1);
+eq2 = theta2_ddot*(m2*r2^2 + I2) - u2 + theta1_ddot*(m2*r2^2 + l1*m2*cos(theta2)*r2 + I2) - g*m2*r2*sin(theta1 + theta2) + l1*m2*r2*theta1_dot*sin(theta2)*(theta1_dot + theta2_dot) - l1*m2*r2*theta1_dot*theta2_dot*sin(theta2);
 
 G1 = subs(eq1, [theta1_ddot, theta1_dot, theta2_ddot, theta2_dot], [0, 0, 0, 0]);
 G2 = subs(eq2, [theta1_ddot, theta1_dot, theta2_ddot, theta2_dot], [0, 0, 0, 0]);
 
-M1 = subs(eq1, [theta1_dot, theta2_dot], [0, 0])
-M2 = subs(eq2, [theta1_dot, theta2_dot], [0, 0])
+M1 = subs(eq1, [theta1_dot, theta2_dot], [0, 0]);
+M2 = subs(eq2, [theta1_dot, theta2_dot], [0, 0]);
 
-C1 = subs(eq1, [theta1_ddot, theta2_ddot], [0, 0]);
-C2 = subs(eq2, [theta1_ddot, theta2_ddot], [0, 0]);
+C1 = subs(eq1, [theta1_ddot, theta2_ddot], [0, 0]) - G1;
+C2 = subs(eq2, [theta1_ddot, theta2_ddot], [0, 0]) - G2;
 
 M = [m2*l1^2 + 2*m2*cos(theta2)*l2*r2 + m1*r1^2 + m2*r2^2 + I1 + I2, m2*r2^2 + l1*m2*cos(theta2)*r2 + I2; I2 + m2*r2^2+ m2*r2*l1*cos(theta2), m2*r2^2 + I2] ;
-C= [0,l1*m2*r2*theta1_dot*sin(theta2) + l1*m2*r2*sin(theta2)*(theta1_dot + theta2_dot); l1*m2*r2*(theta1_dot + theta2_dot)*sin(theta2) - l1*m1*r2*theta2_dot*sin(theta2), 0];
+C= [l1*m2*r2*sin(theta2), -l1*m2*r2*theta1_dot*sin(theta2) + l1*m2*r2*sin(theta2); l1*m2*r2*sin(theta2)*(theta1_dot + theta2_dot) - l1*m2*r2*theta2_dot*sin(theta2), 0];
 G = [- g*l1*m2*sin(theta1) - g*m1*r1*sin(theta1) - m2*g*r2*sin(theta1+theta2); - g*m2*r2*sin(theta1 + theta2)];
 
-M(:, 1)
+% simplify(M*[theta1_ddot; theta2_ddot] + C*[theta1_dot; theta2_dot] + G - [eq1; eq2])
+% simplify([C1; C2] - C*[theta1_dot; theta2_dot])
 
-lambda = [-1, -2, -3, -4];
+% lambda = [-1, -2, -3, -4];
+lambda = [-2, -3, -4, -5];
 A = [0, 0, 1, 0; 0, 0, 0, 1; 0, 0, 0, 0; 0, 0, 0, 0];
 B = [0, 0; 0, 0; 1, 0; 0, 1];
-K = place(A, B, lambda);
+K = place(A, B, lambda)
 
 % Simulation of the system 
 T = 10;
-% [t,y] = ode45(@ode_link, [0,T], [deg2rad(200),deg2rad(125),0,0]);
-[t,y] = ode45(@ode_link, [0,T], [pi, pi/2, 0,0]);
+[t,y] = ode45(@ode_link, [0,T], [deg2rad(200),deg2rad(125),0, 0]);
+% [t,y] = ode45(@ode_link, [0,T], [pi, pi/2, 0, 0]);
+% [t,y] = ode45(@ode_link, [0,T], [0, 0, 0, 0]);
 
 K = [12, 0 , 7, 0; 0, 2, 0, 3];
 
@@ -57,26 +58,27 @@ q1ddot_d = - (6*pi)/100 + (6.*t*pi)/500;
 q2_d =  pi/2 - (3*pi.*t.^2)/200 + (pi.*t.^3)/1000;
 q2dot_d = - (6*pi.*t)/200 + (3.*t.^2*pi)/1000;
 q2ddot_d =- (6*pi)/200 + (6.*t*pi)/1000;
-y(1, 1)
-q1_d(1)
+% y(1, 1);
+% q1_d(1)
 
 for i = 1:size(y,1)
     v1(i) = - K(1,1)*(y(i,1) - q1_d(i)) - K(1,2)*(y(i,2) - q2_d(i)) - K(1,3)*(y(i,3) - q1dot_d(i)) - K(1,4)*(y(i,4) - q1dot_d(i)) + q1ddot_d(i);
     v2(i) = - K(2,1)*(y(i,1) - q1_d(i)) - K(2,2)*(y(i,2) - q2_d(i)) - K(2,3)*(y(i,3) - q1dot_d(i)) - K(2,4)*(y(i,4) - q1dot_d(i)) + q2ddot_d(i);
     
     M = [m2*l1^2 + 2*m2*cos(y(i,2))*l2*r2 + m1*r1^2 + m2*r2^2 + I1 + I2, m2*r2^2 + l1*m2*cos(y(i,2))*r2 + I2; I2 + m2*r2^2+ m2*r2*l1*cos(y(i,2)), m2*r2^2 + I2] ;
-    C= [0,l1*m2*r2*y(i,3)*sin(y(i,2)) + l1*m2*r2*sin(y(i,2))*(y(i,3) + y(i,4)); l1*m2*r2*(y(i,3) + y(i,4))*sin(y(i,2)) - l1*m1*r2*y(i,4)*sin(y(i,2)), 0];
+    C= [l1*m2*r2*sin(theta2), -l1*m2*r2*theta1_dot*sin(theta2) + l1*m2*r2*sin(theta2); l1*m2*r2*sin(theta2)*(theta1_dot + theta2_dot) - l1*m2*r2*theta2_dot*sin(theta2), 0];
+    C1 = [-y(i,4)*((9*sin(y(i,2))*(y(i,3) + y(i,4)))/20 + (9*y(i,3)*sin(y(i,2)))/20)];
+    C2 = [(9*y(i,3)*sin(y(i,2))*(y(i,3) + y(i,4)))/20 - (9*y(i,3)*y(i,4)*sin(y(i,2)))/20];
     G = [- g*l1*m2*sin(y(i,1)) - g*m1*r1*sin(y(i,1)) - m2*g*r2*sin(y(i,1) + y(i,2)); - g*m2*r2*sin(y(i,1) + y(i,2))];
     
-    u1(i)= M(1, :) * [v1(i); v2(i)] + (C(1, :) * [y(i,3); y(i,4)]) +G(1, 1) ;
-    u2(i)= M(2, :) * [v1(i); v2(i)] + (C(2, :) * [y(i,3); y(i,4)]) +G(2, 1) ;
+%     u1(i)= M(1, :) * [v1(i); v2(i)] + (C(1, :) * [y(i,3); y(i,4)]) + G(1, 1) ;
+%     u2(i)= M(2, :) * [v1(i); v2(i)] + (C(2, :) * [y(i,3); y(i,4)]) + G(2, 1) ;
+    u1(i)= M(1, :) * [v1(i); v2(i)] + C1 + G(1, 1) ;
+    u2(i)= M(2, :) * [v1(i); v2(i)] + C2 + G(2, 1) ;
 end
 
-% size(u1)
-% size(v1)
-% size(v2)
-
 figure(1)
+subplot(2,2,1);
 plot(t, q1_d);
 xlabel('t', 'FontSize',14)
 ylabel('q1_d','FontSize',14);
@@ -87,7 +89,7 @@ ylabel('theta1','FontSize',14);
 legend('desired','actual')
 hold off
 
-figure(2)
+subplot(2,2,2);
 plot(t, q1dot_d);
 xlabel('t', 'FontSize',14)
 ylabel('q1_d','FontSize',14);
@@ -98,7 +100,7 @@ ylabel('theta1 dot','FontSize',14)
 legend('desired','actual')
 hold off
 
-figure(3)
+subplot(2,2,3);
 plot(t, q2_d);
 xlabel('t', 'FontSize',14)
 ylabel('q2_d','FontSize',14);
@@ -109,7 +111,7 @@ ylabel('theta2','FontSize',14)
 legend('desired','actual')
 hold off
 
-figure(4)
+subplot(2,2,4);
 plot(t, q2dot_d);
 xlabel('t', 'FontSize',14)
 ylabel('q2_d','FontSize',14);
@@ -120,31 +122,7 @@ ylabel('theta2 dot','FontSize',14)
 legend('desired','actual')
 hold off
 
-% figure(2)
-% plot(t,y);
-% 
-% figure(1)
-% subplot(2,2,1);
-% plot(t,y(:,1),'b');
-% xlabel('t', 'FontSize',14)
-% ylabel('theta1','FontSize',14);
-% 
-% subplot(2,2,2);
-% plot(t,y(:,2),'r');
-% xlabel('t', 'FontSize',14)
-% ylabel('theta2','FontSize',14)
-% 
-% subplot(2,2,3);
-% plot(t,y(:,3),'b');
-% xlabel('t', 'FontSize',14)
-% ylabel('theta1 dot','FontSize',14)
-% 
-% subplot(2,2,4);
-% plot(t,y(:,4),'r');
-% xlabel('t', 'FontSize',14)
-% ylabel('theta2 dot','FontSize',14)
-% 
-figure(5)
+figure(2)
 subplot(2, 1, 1);
 plot(t, u1,'b');
 xlabel('t', 'FontSize', 14)
@@ -162,21 +140,10 @@ dX= zeros(4,1);
 X=num2cell(X);
 [theta1, theta2, theta1_dot, theta2_dot] = deal(X{:});
 
-% if abs(theta1) > 2*pi
-%     theta1 = mod(theta1,2*pi);
-% 
-% end
-% 
-% if abs(theta2) > 2*pi
-%     theta2 = mod(theta2,2*pi);
-% end
-
-% Manipulator form
-% eq1= theta1_ddot*(m2*l1^2 + 2*m2*cos(theta2)*l1*r2 + m1*r1^2 + m2*r2^2 + I1 + I2) - theta2_dot*(l1*m2*r2*theta1_dot*sin(theta2) + l1*m2*r2*sin(theta2)*(theta1_dot + theta2_dot)) - u1 + theta2_ddot*(m2*r2^2 + l1*m2*cos(theta2)*r2 + I2) - g*m2*r2*sin(theta1 + theta2) - g*l1*m1*sin(theta1) - g*m1*r1*sin(theta1);
-% eq2= theta2_ddot*(m2*r2^2 + I2) - u2 + theta1_ddot*(m2*r2^2 + l1*m2*cos(theta2)*r2 + I2) - g*m2*r2*sin(theta1 + theta2) + l1*m2*r2*theta1_dot*sin(theta2)*(theta1_dot + theta2_dot) - l1*m2*r2*theta1_dot*theta2_dot*sin(theta2);
-
 M = [m2*l1^2 + 2*m2*cos(theta2)*l1*r2 + m1*r1^2 + m2*r2^2 + I1 + I2, m2*r2^2 + l1*m2*cos(theta2)*r2 + I2; I2 + m2*r2^2+ m2*r2*l1*cos(theta2), m2*r2^2 + I2] ;
-C= [0,l1*m2*r2*theta1_dot*sin(theta2) + l1*m2*r2*sin(theta2)*(theta1_dot + theta2_dot); l1*m2*r2*(theta1_dot + theta2_dot)*sin(theta2) - l1*m1*r2*theta2_dot*sin(theta2), 0];
+C= [l1*m2*r2*sin(theta2), -l1*m2*r2*theta1_dot*sin(theta2) + l1*m2*r2*sin(theta2); l1*m2*r2*sin(theta2)*(theta1_dot + theta2_dot) - l1*m2*r2*theta2_dot*sin(theta2), 0];
+C1 = [-theta2_dot*((9*sin(theta2)*(theta1_dot + theta2_dot))/20 + (9*theta1_dot*sin(theta2))/20)];
+C2 = [(9*theta1_dot*sin(theta2)*(theta1_dot + theta2_dot))/20 - (9*theta1_dot*theta2_dot*sin(theta2))/20];
 G = [- g*l1*m2*sin(theta1) - g*m1*r1*sin(theta1) - m2*g*r2*sin(theta1 + theta2); - g*m2*r2*sin(theta1 + theta2)];
 
 q1_d = pi - (3*pi*t^2)/100 + (pi*t^3)/500;
@@ -189,8 +156,8 @@ q2ddot_d = - (6*pi)/200 + (6*t*pi)/1000;
 
 % Feedback linearization control 
 K = [12, 0 , 7, 0; 0, 2, 0, 3];
-
-U = M*(- K*([theta1; theta2; theta1_dot; theta2_dot] - [q1_d; q2_d; q1dot_d; q2dot_d]) +[q1ddot_d; q2ddot_d]) + C*[theta1_dot;theta2_dot] + G;
+% K = [9, 0 , 5, 0; 0, 1.5, 0, 2.25];
+U = M*(- K*([theta1; theta2; theta1_dot; theta2_dot] - [q1_d; q2_d; q1dot_d; q2dot_d]) +[q1ddot_d; q2ddot_d]) + [C1; C2] + G;
 
 u1 = [U(1,:)];
 u2 = [U(2,:)];
